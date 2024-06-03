e15ce33a3a8ffc61a1981f69c386c5dde3aee2af
AngeLSanchez210 <is21001@ues.edu.sv>
2024-06-03 12:21:27 -0600

    Comentarios user

--- Destinos/app/src/main/java/com/example/destinos/Adapter/destinoAdapater.java
+++ Destinos/app/src/main/java/com/example/destinos/Adapter/destinoAdapater.java
@@ -1,6 +1,7 @@
 package com.example.destinos.Adapter;
 
 import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -18,6 +19,7 @@ import com.example.destinos.Destinos;
 import com.example.destinos.R;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -30,16 +32,13 @@ import java.util.ArrayList;
 
 public class destinoAdapater extends BaseAdapter {
 
-    public Context context;
-
-    public ArrayList<Destinos> dataDestinos;
-
-    public DatabaseReference reference;
+    private Context context;
+    private ArrayList<Destinos> dataDestinos;
+    private DatabaseReference reference;
     private FirebaseAuth mauth;
-    Comentarios comentario;
-    public String destinoId;
-    public String nombreDestino;
-    public String nombreUsuario;
+    private Comentarios comentario;
+    private String destinoId;
+    private String nombreDestino;
 
     public destinoAdapater(Context context, ArrayList<Destinos> dataDestinos) {
         this.context = context;
@@ -64,15 +63,14 @@ public class destinoAdapater extends BaseAdapter {
 
     @Override
     public View getView(int position, View view, ViewGroup parent) {
-
-        view = LayoutInflater.from(context).inflate(R.layout.item_list,null);
+        view = LayoutInflater.from(context).inflate(R.layout.item_list, null);
 
         Destinos ds = dataDestinos.get(position);
 
-        TextView txtnombre= view.findViewById(R.id.lblnameD);
-        TextView txtdes= view.findViewById(R.id.lbldesD);
-        TextView txtdire= view.findViewById(R.id.lbldireD);
-        ImageView img= view.findViewById(R.id.imageView3);
+        TextView txtnombre = view.findViewById(R.id.lblnameD);
+        TextView txtdes = view.findViewById(R.id.lbldesD);
+        TextView txtdire = view.findViewById(R.id.lbldireD);
+        ImageView img = view.findViewById(R.id.imageView3);
 
         txtnombre.setText(ds.getNombre());
         txtdes.setText(ds.getDescripcion());
@@ -86,21 +84,20 @@ public class destinoAdapater extends BaseAdapter {
         EditText cajaComentarios = view.findViewById(R.id.txtComentarios);
         EditText cajaPuntuacion = view.findViewById(R.id.txtPuntuacion);
         //----------------------------------------------------
-        //establecemos el button de mandar comentario y la caja de comentarios de manera invisible
-        btnEnviar.setVisibility(view.INVISIBLE);
-        cajaComentarios.setVisibility(view.INVISIBLE);
-        cajaPuntuacion.setVisibility(view.INVISIBLE);
+        // Establecemos el botón de enviar comentario y la caja de comentarios de manera invisible
+        btnEnviar.setVisibility(View.INVISIBLE);
+        cajaComentarios.setVisibility(View.INVISIBLE);
+        cajaPuntuacion.setVisibility(View.INVISIBLE);
         //---------------------------------------------------
-        View finalView = view;
-        //Eventos
+        // Eventos
         btnComentarios.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                btnEnviar.setVisibility(finalView.VISIBLE);
-                cajaComentarios.setVisibility(finalView.VISIBLE);
-                cajaPuntuacion.setVisibility(finalView.VISIBLE);
+                btnEnviar.setVisibility(View.VISIBLE);
+                cajaComentarios.setVisibility(View.VISIBLE);
+                cajaPuntuacion.setVisibility(View.VISIBLE);
                 nombreDestino = ds.Direccion;
-                //Toast.makeText(context, nombreDestino, Toast.LENGTH_SHORT).show();
+                // Toast.makeText(context, nombreDestino, Toast.LENGTH_SHORT).show();
             }
         });
         btnEnviar.setOnClickListener(new View.OnClickListener() {
@@ -110,50 +107,74 @@ public class destinoAdapater extends BaseAdapter {
                 reference.keepSynced(true);
                 mauth = FirebaseAuth.getInstance();
 
-                //Defiimos una consulta para encontrar el nombre de un lugar
+                // Definimos una consulta para encontrar el nombre de un lugar
                 Query query = reference.orderByChild("Nombre").equalTo(nombreDestino);
                 query.addListenerForSingleValueEvent(new ValueEventListener() {
                     @Override
                     public void onDataChange(@NonNull DataSnapshot snapshot) {
-                        for(DataSnapshot destinoSnapshot : snapshot.getChildren()){
+                        for (DataSnapshot destinoSnapshot : snapshot.getChildren()) {
                             destinoId = destinoSnapshot.getKey();
-                            //Toast.makeText(context, destinoId, Toast.LENGTH_SHORT).show();
+                        }
+
+                        // Validamos que no hayan campos vacíos
+                        if (cajaComentarios.getText().toString().isEmpty() && cajaPuntuacion.getText().toString().isEmpty()) {
+                            Toast.makeText(context, "Aun no has comentado nada viajero", Toast.LENGTH_SHORT).show();
+                        } else {
+                            comentario.comment = cajaComentarios.getText().toString();
+                            comentario.puntuacion = Integer.parseInt(cajaPuntuacion.getText().toString());
+
+                            // Asignamos el id del usuario logueado al comentario
+                            FirebaseUser currentUser = mauth.getCurrentUser();
+                            if (currentUser != null) {
+                                String id = currentUser.getUid();
+                                comentario.idUser = id;
+
+                                // Obtenemos el nombre del usuario actual y lo asignamos al comentario
+                                DatabaseReference userRef = FirebaseDatabase.getInstance().getReference().child("Usuarios").child(id);
+                                userRef.addListenerForSingleValueEvent(new ValueEventListener() {
+                                    @Override
+                                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                                        if (dataSnapshot.exists()) {
+                                            String nombreUsuario = dataSnapshot.child("nameUser").getValue(String.class);
+                                            comentario.nameUsuario = nombreUsuario;
+
+
+
+                                            Toast.makeText(context, "Comentario enviado por: " + nombreUsuario, Toast.LENGTH_SHORT).show();
+
+                                            comentario.idDestino = destinoId;
+                                            comentario.nameDestino = nombreDestino;
+
+                                            // Guardamos el comentario en la base de datos
+                                            reference.child("Comentario").push().setValue(comentario).addOnSuccessListener(new OnSuccessListener<Void>() {
+                                                @Override
+                                                public void onSuccess(Void unused) {
+                                                    Toast.makeText(context, "Comentario enviado", Toast.LENGTH_SHORT).show();
+                                                }
+                                            });
+                                        }
+                                    }
+
+                                    @Override
+                                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                                    }
+                                });
+                            } else {
+                                // Si no hay usuario autenticado, muestra un mensaje de error
+                                Toast.makeText(context, "Error: No se ha encontrado un usuario autenticado", Toast.LENGTH_SHORT).show();
+                            }
                         }
                     }
 
                     @Override
                     public void onCancelled(@NonNull DatabaseError error) {
-
+                       
                     }
                 });
-
-                //Validamos que no hayan campos vacios
-                if(cajaComentarios.getText().toString().isEmpty() && cajaPuntuacion.getText().toString().isEmpty()){
-                    Toast.makeText(context, "Aun no has comentado nada viajero", Toast.LENGTH_SHORT).show();
-                }
-                else{
-                    comentario.comment = cajaComentarios.getText().toString();
-                    comentario.puntuacion = Integer.parseInt(cajaPuntuacion.getText().toString());
-                    //Asigamos el id del usuario loguado al comentario
-                    String id = mauth.getCurrentUser().getUid();
-                    comentario.idUser = id;
-
-                    comentario.idDestino = destinoId;
-                    comentario.nameDestino = nombreDestino;
-                    comentario.nameUsuario = nombreUsuario;
-
-                    reference.child("Comentario").push().setValue(comentario).addOnSuccessListener(new OnSuccessListener<Void>() {
-                        @Override
-                        public void onSuccess(Void unused) {
-                            Toast.makeText(context, "Comentario enviado", Toast.LENGTH_SHORT).show();
-                        }
-                    });
-
-
-                }
             }
-
         });
+
 
         return view;
     }
